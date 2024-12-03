1a7a7b99963d52ecb1889353cf0c87eada68e265
David Rouyer <rouyer.david@gmail.com>
2024-12-03 22:42:27 +0100

    feat(ui) introduce ui package

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -1,12 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/start/config";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     preset: "node-server",
+    compatibilityDate: "2024-12-02",
   },
   vite: {
     plugins: [
+      tailwindcss(),
       viteTsConfigPaths({
         projects: ["./tsconfig.json"],
       }),
--- apps/web/app/routes/index.tsx
+++ apps/web/app/routes/index.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { Button } from "@kyakujs/ui/button";
+
 export const Route = createFileRoute("/")({
   component: Home,
 });
 
 function Home() {
-  return <div className="bg-backround">Home</div>;
+  return (
+    <div className="bg-backround">
+      Home
+      <Button type="button">Click me</Button>
+    </div>
+  );
 }
