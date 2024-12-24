52249457546f27444a01d83e30eeb26b483641a2
David Rouyer <rouyer.david@gmail.com>
2024-12-24 16:40:10 +0100

    chore: cleanup

--- apps/web/src/libs/auth-client.ts
+++ apps/web/src/libs/auth-client.ts
@@ -5,7 +5,6 @@ import { authQueryOptions } from "~/services/auth.query";
 export const authClient = createAuthClient({
   ...authClientOptions,
   fetchOptions: {
-    // https://discord.com/channels/1288403910284935179/1288403910284935182/1311199374793244703
     onResponse: async () => {
       await window.getQueryClient().invalidateQueries(authQueryOptions());
       await window.getRouter().invalidate();
