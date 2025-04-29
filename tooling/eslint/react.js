b904df0bc3ac3c79fc21728b6156ef52607fe275
David Rouyer <rouyer.david@gmail.com>
2025-04-29 12:04:19 +0200

    feat(eslint): React Compiler RC

--- tooling/eslint/react.js
+++ tooling/eslint/react.js
@@ -1,25 +1,22 @@
 import routerPlugin from "@tanstack/eslint-plugin-router";
 import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import reactPlugin from "eslint-plugin-react";
-import reactCompiler from "eslint-plugin-react-compiler";
-import hooksPlugin from "eslint-plugin-react-hooks";
+import * as reactHooks from "eslint-plugin-react-hooks";
 
 /** @type {Awaited<import('typescript-eslint').Config>} */
 export default [
+  reactHooks.configs.recommended,
   {
     files: ["**/*.ts", "**/*.tsx"],
     plugins: {
       "@tanstack/router": routerPlugin,
       react: reactPlugin,
-      "react-compiler": reactCompiler,
-      "react-hooks": hooksPlugin,
       "jsx-a11y": jsxA11yPlugin,
     },
     rules: {
       ...routerPlugin.configs.recommended.rules,
       ...reactPlugin.configs["jsx-runtime"].rules,
-      "react-compiler/react-compiler": "error",
-      ...hooksPlugin.configs.recommended.rules,
+      "react-hooks/react-compiler": "error",
       ...jsxA11yPlugin.configs.recommended.rules,
     },
     languageOptions: {
