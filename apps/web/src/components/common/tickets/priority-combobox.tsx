ceaed5cccde29dd603a76a5dd0a6265f51e49564
David Rouyer <rouyer.david@gmail.com>
2025-09-15 18:13:05 +0200

    chore: cleanup

--- apps/web/src/components/common/tickets/priority-combobox.tsx
+++ apps/web/src/components/common/tickets/priority-combobox.tsx
@@ -32,7 +32,7 @@ function CustomCombobox(props: {
         console.log(value);
       }}
     >
-      <ComboboxTrigger className="h-8 cursor-default items-center justify-between rounded-lg border border-gray-200 bg-[canvas] bg-clip-padding px-3 text-sm text-gray-900 select-none hover:bg-gray-50 focus-visible:outline focus-visible:-outline-offset-1 focus-visible:outline-blue-800 data-[popup-open]:bg-gray-50">
+      <ComboboxTrigger>
         <ComboboxValue>
           {(priority: Priority) => (
             <div className="flex items-center gap-2">
@@ -50,30 +50,29 @@ function CustomCombobox(props: {
           trackAnchor={false}
         >
           <ComboboxPopup
-            className="max-h-[min(24rem,var(--available-height))] max-w-[15rem] origin-[var(--transform-origin)] rounded-lg bg-[canvas] bg-clip-padding text-gray-900 shadow-[0_1px_2px_rgba(0,0,0,.025),_0_1px_3px_rgba(0,0,0,.025)] shadow-gray-200 outline-1 outline-gray-200 transition-[transform,scale,opacity] [--input-container-height:3rem] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300"
+            className="max-h-[min(24rem,var(--available-height))] max-w-[15rem] origin-[var(--transform-origin)]"
             aria-label="Select priority"
+            style={{ "--row-width": "15rem" } as React.CSSProperties}
           >
-            <div className="grid w-60 grid-cols-[1fr_auto] gap-2 p-1 pr-3 pl-3.5 text-center">
+            <div className="grid w-(--row-width) grid-cols-[1fr_auto] gap-2 p-1 pr-3 pl-3.5 text-center">
               <ComboboxInput
                 placeholder="Set priority to..."
-                className="col-start-1 h-8 w-full text-sm font-normal text-gray-900 outline-none"
+                className="col-start-1"
               />
               <span className="col-start-2 inline-flex items-center justify-center whitespace-nowrap">
-                <kbd className="min-w-4.5 rounded-md border border-gray-300 p-0.5 text-xs leading-[1.1] text-gray-800">
+                <kbd className="min-w-4.5 rounded-sm border border-input p-0.5 text-xs leading-[1.1]">
                   P
                 </kbd>
               </span>
             </div>
-            <ComboboxSeparator className="border-t border-gray-200" />
-            <ComboboxEmpty className="p-4 text-[0.925rem] leading-4 text-gray-600 empty:m-0 empty:p-0">
-              No priority found.
-            </ComboboxEmpty>
-            <ComboboxList className="max-h-[min(calc(24rem-var(--input-container-height)),calc(var(--available-height)-var(--input-container-height)))] scroll-py-2 overflow-y-auto overscroll-contain py-2 empty:p-0">
+            <ComboboxSeparator />
+            <ComboboxEmpty>No priority found.</ComboboxEmpty>
+            <ComboboxList className="max-h-[min(calc(24rem-var(--input-container-height)),calc(var(--available-height)-var(--input-container-height)))]">
               {(priority: Priority) => (
                 <ComboboxItem
                   key={priority.value}
                   value={priority}
-                  className="inline-flex min-w-[var(--anchor-width)] cursor-default items-center gap-2 py-2 pr-3 pl-3.5 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-800 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-100"
+                  className="w-(--row-width)"
                 >
                   <div>
                     <priority.icon className="size-4 text-muted-foreground" />
--- packages/ui/src/combobox.tsx
+++ packages/ui/src/combobox.tsx
@@ -1,5 +1,7 @@
 import { Combobox as ComboboxPrimitive } from "@base-ui-components/react";
 
+import { cn } from "@kyakujs/ui";
+
 const Combobox = ComboboxPrimitive.Root;
 
 const ComboboxValue: React.FC<
@@ -13,9 +15,17 @@ ComboboxValue.displayName = "ComboboxValue";
 
 const ComboboxTrigger: React.FC<
   React.ComponentProps<typeof ComboboxPrimitive.Trigger>
-> = ({ children, ...props }) => {
+> = ({ children, className, ...props }) => {
   return (
-    <ComboboxPrimitive.Trigger {...props}>{children}</ComboboxPrimitive.Trigger>
+    <ComboboxPrimitive.Trigger
+      className={cn(
+        className,
+        "inline-flex h-8 cursor-default items-center justify-between rounded-md border border-input bg-transparent px-3 text-sm text-foreground select-none focus-visible:outline-sidebar-ring data-[popup-open]:bg-input/50 dark:bg-input/30 dark:hover:bg-input/50",
+      )}
+      {...props}
+    >
+      {children}
+    </ComboboxPrimitive.Trigger>
   );
 };
 ComboboxTrigger.displayName = "ComboboxTrigger";
@@ -42,27 +52,43 @@ ComboboxPositioner.displayName = "ComboboxPositioner";
 
 const ComboboxPopup: React.FC<
   React.ComponentProps<typeof ComboboxPrimitive.Popup>
-> = ({ children, ...props }) => {
+> = ({ children, className, ...props }) => {
   return (
-    <ComboboxPrimitive.Popup {...props}>{children}</ComboboxPrimitive.Popup>
+    <ComboboxPrimitive.Popup
+      className={cn(
+        className,
+        "rounded-md bg-popover text-foreground shadow-lg outline-1 outline-input transition-[transform,scale,opacity] [--input-container-height:3rem] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0",
+      )}
+      {...props}
+    >
+      {children}
+    </ComboboxPrimitive.Popup>
   );
 };
 ComboboxPopup.displayName = "ComboboxPopup";
 
 const ComboboxInput: React.FC<
   React.ComponentProps<typeof ComboboxPrimitive.Input>
-> = ({ children, ...props }) => {
+> = ({ children, className, ...props }) => {
   return (
-    <ComboboxPrimitive.Input {...props}>{children}</ComboboxPrimitive.Input>
+    <ComboboxPrimitive.Input
+      className={(cn(className), "h-8 w-full text-sm font-normal outline-none")}
+      {...props}
+    >
+      {children}
+    </ComboboxPrimitive.Input>
   );
 };
 ComboboxInput.displayName = "ComboboxInput";
 
 const ComboboxSeparator: React.FC<
   React.ComponentProps<typeof ComboboxPrimitive.Separator>
-> = ({ children, ...props }) => {
+> = ({ children, className, ...props }) => {
   return (
-    <ComboboxPrimitive.Separator {...props}>
+    <ComboboxPrimitive.Separator
+      className={cn(className, "border-t bg-border")}
+      {...props}
+    >
       {children}
     </ComboboxPrimitive.Separator>
   );
@@ -71,24 +97,52 @@ ComboboxSeparator.displayName = "ComboboxSeparator";
 
 const ComboboxEmpty: React.FC<
   React.ComponentProps<typeof ComboboxPrimitive.Empty>
-> = ({ children, ...props }) => {
+> = ({ children, className, ...props }) => {
   return (
-    <ComboboxPrimitive.Empty {...props}>{children}</ComboboxPrimitive.Empty>
+    <ComboboxPrimitive.Empty
+      className={cn(
+        className,
+        "p-4 text-sm leading-4 text-muted-foreground empty:m-0 empty:p-0",
+      )}
+      {...props}
+    >
+      {children}
+    </ComboboxPrimitive.Empty>
   );
 };
 ComboboxEmpty.displayName = "ComboboxEmpty";
 
 const ComboboxList: React.FC<
   React.ComponentProps<typeof ComboboxPrimitive.List>
-> = ({ children, ...props }) => {
-  return <ComboboxPrimitive.List {...props}>{children}</ComboboxPrimitive.List>;
+> = ({ children, className, ...props }) => {
+  return (
+    <ComboboxPrimitive.List
+      className={cn(
+        className,
+        "scroll-py-1 overflow-y-auto overscroll-contain py-1 empty:p-0",
+      )}
+      {...props}
+    >
+      {children}
+    </ComboboxPrimitive.List>
+  );
 };
 ComboboxList.displayName = "ComboboxList";
 
 const ComboboxItem: React.FC<
   React.ComponentProps<typeof ComboboxPrimitive.Item>
-> = ({ children, ...props }) => {
-  return <ComboboxPrimitive.Item {...props}>{children}</ComboboxPrimitive.Item>;
+> = ({ children, className, ...props }) => {
+  return (
+    <ComboboxPrimitive.Item
+      {...props}
+      className={cn(
+        className,
+        "inline-flex items-center gap-2 py-2 pr-3 pl-3.5 text-sm leading-4 outline-none select-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-accent-foreground data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-accent",
+      )}
+    >
+      {children}
+    </ComboboxPrimitive.Item>
+  );
 };
 ComboboxItem.displayName = "ComboboxItem";
 
