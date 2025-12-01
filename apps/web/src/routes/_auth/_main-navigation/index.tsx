d37643f8851e8407ba0afbb8294c30d6b1051e42
David Rouyer <rouyer.david@gmail.com>
2025-12-01 20:40:37 +0100

    fix: build

--- apps/web/src/routes/_auth/_main-navigation/index.tsx
+++ apps/web/src/routes/_auth/_main-navigation/index.tsx
@@ -5,7 +5,7 @@ export const Route = createFileRoute("/_auth/_main-navigation/")({
   beforeLoad: () => {
     // eslint-disable-next-line @typescript-eslint/only-throw-error
     throw redirect({
-      to: "/tickets/todo",
+      to: "/tickets/all",
     });
   },
 });
