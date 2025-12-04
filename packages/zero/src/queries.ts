e892c1d3b0641f6dd17a964f4b6a1c48d39a55c7
David Rouyer <rouyer.david@gmail.com>
2025-12-04 21:06:37 +0100

    fix: build

--- packages/zero/src/queries.ts
+++ packages/zero/src/queries.ts
@@ -7,24 +7,33 @@ export const queries = {
   ticket: syncedQuery("ticket", z.tuple([z.string()]), (ticketId: string) =>
     builder.ticket.related("timelineEntries").where("id", ticketId).one(),
   ),
-  tickets: syncedQuery("tickets", z.tuple([z.object({
-    filters: z.object({
-      assignees: z.array(z.string()).optional(),
-      statuses: z.array(z.number()).optional(),
-    })
-  })]), ({ filters }) => {
-    let q = builder.ticket;
-    q = q.where(({and, cmp }) => and(
-      filters.statuses ? cmp('status', 'IN', filters.statuses): undefined,
-       filters.assignees ?
-          cmp('assignedToId', 'IN', filters.assignees ?? []) : undefined
-        )
+  tickets: syncedQuery(
+    "tickets",
+    z.tuple([
+      z.object({
+        filters: z.object({
+          assignees: z.array(z.string()).optional(),
+          statuses: z.array(z.number()).optional(),
+        }),
+      }),
+    ]),
+    ({ filters }) => {
+      let q = builder.ticket;
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      q = q.where(({ and, cmp }) =>
+        and(
+          filters.statuses ? cmp("status", "IN", filters.statuses) : undefined,
+          filters.assignees
+            ? cmp("assignedToId", "IN", filters.assignees ?? [])
+            : undefined,
+        ),
       );
-    return q
-      .related("assignedTo", (assignee) => assignee.one())
-      .related("customer")
-      .related("labels")
-      .orderBy("priority", "asc")
-      .orderBy("createdAt", "asc");
-  }),
+      return q
+        .related("assignedTo", (assignee) => assignee.one())
+        .related("customer")
+        .related("labels")
+        .orderBy("priority", "asc")
+        .orderBy("createdAt", "asc");
+    },
+  ),
 };
