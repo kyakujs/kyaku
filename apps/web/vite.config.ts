e248cde799a6f9f4ce9902f92d79012bdb39968e
David Rouyer <rouyer.david@gmail.com>
2025-12-18 07:10:50 +0100

    chore(dev-deps): bump Vite to 8.0.0-beta.3

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,13 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    tsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
     tanstackStart({
       router: {
         generatedRouteTree: "./route-tree.gen.ts",
@@ -21,4 +17,7 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
