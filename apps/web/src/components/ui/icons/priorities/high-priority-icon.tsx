0515df47714c5dd0cd020f6ece139a72c25d6fba
David Rouyer <rouyer.david@gmail.com>
2025-12-18 20:15:53 +0100

    chore: cleanup

--- apps/web/src/components/ui/icons/priorities/high-priority-icon.tsx
+++ apps/web/src/components/ui/icons/priorities/high-priority-icon.tsx
@@ -1,25 +1,23 @@
-const HighPriorityIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
+export function HighPriorityIcon({
   className,
   ...props
-}) => (
-  <svg
-    width="16"
-    height="16"
-    viewBox="0 0 16 16"
-    fill="currentColor"
-    className={className}
-    aria-label="High Priority"
-    role="img"
-    focusable="false"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <rect x="1.5" y="8" width="3" height="6" rx="1"></rect>
-    <rect x="6.5" y="5" width="3" height="9" rx="1"></rect>
-    <rect x="11.5" y="2" width="3" height="12" rx="1"></rect>
-  </svg>
-);
-
-HighPriorityIcon.displayName = "HighPriorityIcon";
-
-export { HighPriorityIcon };
+}: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      className={className}
+      aria-label="High Priority"
+      role="img"
+      focusable="false"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect x="1.5" y="8" width="3" height="6" rx="1"></rect>
+      <rect x="6.5" y="5" width="3" height="9" rx="1"></rect>
+      <rect x="11.5" y="2" width="3" height="12" rx="1"></rect>
+    </svg>
+  );
+}
--- apps/web/src/components/ui/icons/priorities/low-priority-icon.tsx
+++ apps/web/src/components/ui/icons/priorities/low-priority-icon.tsx
@@ -1,25 +1,30 @@
-const LowPriorityIcon: React.FC<React.SVGProps<SVGSVGElement>> = ({
+export function LowPriorityIcon({
   className,
   ...props
-}) => (
-  <svg
-    width="16"
-    height="16"
-    viewBox="0 0 16 16"
-    fill="currentColor"
-    className={className}
-    aria-label="Low Priority"
-    role="img"
-    focusable="false"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <rect x="1.5" y="8" width="3" height="6" rx="1"></rect>
-    <rect x="6.5" y="5" width="3" height="9" rx="1" fillOpacity="0.4"></rect>
-    <rect x="11.5" y="2" width="3" height="12" rx="1" fillOpacity="0.4"></rect>
-  </svg>
-);
-
-LowPriorityIcon.displayName = "LowPriorityIcon";
-
-export { LowPriorityIcon };
+}: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="currentColor"
+      className={className}
+      aria-label="Low Priority"
+      role="img"
+      focusable="false"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect x="1.5" y="8" width="3" height="6" rx="1"></rect>
+      <rect x="6.5" y="5" width="3" height="9" rx="1" fillOpacity="0.4"></rect>
+      <rect
+        x="11.5"
+        y="2"
+        width="3"
+        height="12"
+        rx="1"
+        fillOpacity="0.4"
+      ></rect>
+    </svg>
+  );
+}
