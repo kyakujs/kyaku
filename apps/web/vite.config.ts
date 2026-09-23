1f9804e3ff812c6b84c0d7ce64fc9d4cce101c77
David Rouyer <rouyer.david@gmail.com>
2026-09-23 11:39:40 +0200

    feat: use native React Compiler

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,6 @@
-import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -11,9 +10,8 @@ export default defineConfig({
         generatedRouteTree: "./route-tree.gen.ts",
       },
     }),
-    react(),
-    babel({
-      presets: [reactCompilerPreset()],
+    react({
+      compiler: true
     }),
     tailwindcss(),
   ],
